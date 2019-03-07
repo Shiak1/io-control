28b034b2c1202c8df1302020f195827aef396f7c
Shiya Kohn <shiyak11211@gmail.com>
2019-03-07 08:59:06 -0500

    Crash process if no mongo found

--- install/boot.js
+++ install/boot.js
@@ -4,10 +4,21 @@ const process = require('process');
 const db = require('../services/db');
 
 const noMongoFound =
-	'mongodb is reqiuired to run this project,\
-please configure and run npm install again.';
+    'mongodb is reqiuired to run this project,\
+ please configure and run npm install again.';
 
-db()
-	.then(async () => await createAdmin())
-	.catch(() => console.error(noMongoFound))
-	.then(() => process.exit());
+async function init() {
+    try {
+        await db();
+    } catch (_) {
+        console.error(noMongoFound);
+
+        process.exit(1);
+    }
+
+    await createAdmin();
+
+    process.exit();
+}
+
+init();
