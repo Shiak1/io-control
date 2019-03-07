913933b7b7a02743a42f0beeafdb30d8cb5dc5c7
Shiya Kohn <shiyak11211@gmail.com>
2019-03-07 09:11:17 -0500

    Indent with spaces

--- models/user.js
+++ models/user.js
@@ -1,19 +1,19 @@
 const { Schema } = require('mongoose');
 
 module.exports = new Schema({
-	username: { type: String, unique: true, required: true },
-	hash: { type: String, required: true },
-	name: {
-		first: { type: String },
-		last: { type: String },
-	},
-	meta: {
-		createdAt: { type: Date, default: Date.now },
-	},
-	role: {
-		type: String,
-		enum: ['Admin', 'Manager', 'User'],
-		required: true,
-		default: 'User',
-	},
+    username: { type: String, unique: true, required: true },
+    hash: { type: String, required: true },
+    name: {
+        first: { type: String },
+        last: { type: String },
+    },
+    meta: {
+        createdAt: { type: Date, default: Date.now },
+    },
+    role: {
+        type: String,
+        enum: ['Admin', 'Manager', 'User'],
+        required: true,
+        default: 'User',
+    },
 });
