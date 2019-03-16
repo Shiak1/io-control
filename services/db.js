d4b21dbc172b7df323c47672b7a852de0391fd08
Shiya Kohn <shiyak11211@gmail.com>
2019-03-16 10:58:22 -0400

    Downgrade to mongodb 2.x

--- services/db.js
+++ services/db.js
@@ -1,13 +1,14 @@
 const mongoose = require('mongoose');
 
+mongoose.Promise = global.Promise;
+
 const {
 	db: { URI },
 } = require('../config.json');
 
 module.exports = async function() {
 	await mongoose.connect(URI, {
-		useCreateIndex: true,
-		useNewUrlParser: true,
+		useMongoClient: true,
 	});
 
 	mongoose.Promise = global.Promise;
