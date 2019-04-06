34485f84c70ebbae44e495543a2c02a105f811ba
Shiya Kohn <skohn@goflowapp.com>
2019-04-05 23:23:28 -0400

    Icon component

---
+++ frontend/src/components/Icon.vue
@@ -0,0 +1,19 @@
+<template>
+    <svg
+        viewBox="0 0 8 8"
+        @click="onClick()"
+        v-b-tooltip.hover
+        v-bind="{ class: `icon ${classes || ''}`, title }"
+    >
+        <path v-bind:d="icons[icon]"></path>
+    </svg>
+</template>
+
+<script>
+import icons from '../assets/icons';
+
+export default {
+    data: () => ({ icons }),
+    props: ['icon', 'classes', 'title', 'onClick'],
+};
+</script>
